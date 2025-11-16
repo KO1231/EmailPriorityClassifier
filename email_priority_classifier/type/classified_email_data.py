f368c5462d9b7fa21cf6ef99254ae7be677867dd
Kazuhiro Oka <k.oka@kazutech.jp>
2025-11-17 00:09:19 +0900

    fix: replaceを正規表現のsubに変更

--- email_priority_classifier/type/classified_email_data.py
+++ email_priority_classifier/type/classified_email_data.py
@@ -1,5 +1,6 @@
 import base64
 import json
+import re
 
 _LABEL_REPLACE_DATA = {
     "INBOX": "Inbox",
@@ -37,8 +38,7 @@ class ClassifiedEmailData:
 
     @staticmethod
     def _decode_body(body: str, headers: list[dict]) -> str:
-        return (base64.urlsafe_b64decode(body).decode("utf-8")
-                .replace("\r", " ").replace("\n", " ").replace("\t", " "))
+        return re.sub(r"\s+", " ", base64.urlsafe_b64decode(body).decode("utf-8"))
         """
         for header in headers:
             if header.get("name", "").lower() == 'content-transfer-encoding':
